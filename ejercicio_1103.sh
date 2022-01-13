e923b7ea733feb5bd1b9ea311392f767d98f0332
LeoPRhon <leonardo.proano@est.ikiam.edu.ec>
2022-01-12 23:14:41 -0500

    Update ejercicio_1103.sh
    Primera pregunta del ejercicio 1.10.4

--- ejercicio_1103.sh
+++ ejercicio_1103.sh
@@ -1,2 +1,9 @@
 # En Primer lugar ubicamos la carpeta Saavedra2013
 cd Saavedra2013
+
+# Pregunta 1 del ejercicio
+echo "El ńumero de columnas es:" ;
+head -n1 n1.txt | grep -o " " | wc -l ;
+echo "El numero de filas es:" ;
+wc -l n1.txt; 
+echo "Fin de la ejecucion"
