b3ec0cb41c05ae3d8023346dd017a6438d6c383e
LeoPRhon <leonardo.proano@est.ikiam.edu.ec>
2022-01-12 23:43:57 -0500

    Update ejercicio_1103.sh
    Pregunta 2 del ejercicio

--- ejercicio_1103.sh
+++ ejercicio_1103.sh
@@ -9,3 +9,12 @@ wc -l n1.txt;
 echo "Fin de la ejecucion"
 
 # Pregunta 2 del ejercicio
+cd Saavedra2013
+for f in $(ls *.txt);
+do
+    echo "El numero de columas es:" ;
+    head -n 1 $f | awk '{print NF}'
+    echo "El numero de filas es:" ;
+    cat $f | wc -l
+done
+echo "Fin de la ejecucion"
