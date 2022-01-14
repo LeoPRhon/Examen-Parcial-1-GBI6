fdea8eee6241c5a56ea19198fbff6896b8f71409
LeoPRhon <leonardo.proano@est.ikiam.edu.ec>
2022-01-14 14:04:34 -0500

    Update ejercicio_1104.sh

--- ejercicio_1104.sh
+++ ejercicio_1104.sh
@@ -1,1 +1,7 @@
 # Pregunta 1 del ejercicio 1.10.4
+# 1) Escribe un script que, para un archivo csv y número de columna, imprima:
+#	Nombre de la columna#
+	#Número de valores distintos#
+	#Valor mínimo
+	#Valor máximo #
+
