7c663492dfb02a9ea69cadd55d0a2d72819e7f0b
LeoPRhon <leonardo.proano@est.ikiam.edu.ec>
2022-01-15 10:05:17 -0500

    Ejercicio 2 resuelto Proaño Leonardo

---
+++ Examen Parcial Ej. 2/Ejercicio 2.sh
@@ -0,0 +1,14 @@
+pwd
+
+mkdir test1
+
+touch test1/test1.txt
+
+touch test1/test2.txt
+echo "Hola GBI6" >> test1/test2.txt
+
+touch test1/test1.py
+echo "print('Hola GBI6')" >> test1/test1.py
+
+echo "El examen parcial de GBI6 es hoy" >> test1/test2.txt
+cat test1/test2.txt
