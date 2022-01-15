25731d342152044978047cdd90b14f510313245b
LeoPRhon <leonardo.proano@est.ikiam.edu.ec>
2022-01-14 22:10:21 -0500

    Ejercicio 5 resuelto Proaño Leonardo

--- ejercicio_1104.sh
+++ ejercicio_1104.sh
@@ -5,3 +5,18 @@
 	#Valor mínimo
 	#Valor máximo #
 
+# Nombre de la columna
+
+cut -d ',' -f 7 ../data/Buzzard2015_data.csv | head -n 1
+
+#Número de valores distintos 
+
+cut -d ',' -f 7 ../data/Buzzard2015_data.csv | tail -n +2 | sort | uniq | wc -l
+
+# Valor mínimo
+
+cut -d ',' -f 7 ../data/Buzzard2015_data.csv | tail -n +2 | sort -n | head -n 1
+
+# Valor máximo
+
+cut -d ',' -f 7 ../data/Buzzard2015_data.csv | tail -n +2 | sort -n | tail -n 1
